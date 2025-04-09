3acdd917300de930ab98230e1a42c12a45d81a76
unknown <tirotta39@gmail.com>
2025-04-09 17:27:58 +0200

    Esercizi metodi classe Nazione

--- Giorno3/src/it/prova/model/Nazione.java
+++ Giorno3/src/it/prova/model/Nazione.java
@@ -4,4 +4,84 @@ public class Nazione {
     private String denominazione;
     private double superficieKmQ;
     private int abitanti;
+
+    public Nazione(String denominazione, double superficieKmQ, int abitanti) {
+        this.denominazione = denominazione;
+        this.superficieKmQ = superficieKmQ;
+        this.abitanti = abitanti;
+    }
+
+    public String getDenominazione() {
+        return denominazione;
+    }
+
+    public double getSuperficieKmQ() {
+        return superficieKmQ;
+    }
+
+    public int getAbitanti() {
+        return abitanti;
+    }
+
+    public void setDenominazione(String denominazione) {
+        this.denominazione = denominazione;
+    }
+
+    public void setAbitanti(int abitanti) {
+        this.abitanti = abitanti;
+    }
+
+    public void setSuperficieKmQ(double superficieKmQ) {
+        this.superficieKmQ = superficieKmQ;
+    }
+
+    public boolean piuEstesaDi(Nazione input){
+        return this.superficieKmQ > input.getSuperficieKmQ();
+    }
+    public boolean piuPopolosaDi(Nazione input){
+        return this.abitanti > input.getAbitanti();
+    }
+    public boolean esisteAlmenoUnaPiuEstesa(Nazione[] nazioni){
+        for(int i = 0; i < nazioni.length; i++){
+            if(this.superficieKmQ < nazioni[i].getSuperficieKmQ()){
+                return true;
+            }
+        }
+        return false;
+    }
+    public int dimmiQuanteSonoPiuPopolose(Nazione[] nazioni){
+        int count = 0;
+        for(int i = 0; i < nazioni.length; i++){
+            if(this.abitanti < nazioni[i].getAbitanti()){
+                count += 1;
+            }
+        }
+        return count;
+    }
+    public boolean haPiuAbitantiDiTutteLeAltre(Nazione[] nazioni){
+        for(int i = 0; i < nazioni.length; i++){
+            if(this.abitanti < nazioni[i].getAbitanti()){
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean hannoTutteLaMiaStessaIniziale(Nazione[] nazioni){
+        for(int i = 0; i < nazioni.length; i++){
+            if(this.denominazione.charAt(0) != nazioni[i].getDenominazione().charAt(0)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean eAlDiSopraDellaMediaComeSuperficie(Nazione[] nazioni){
+        double somma = 0;
+        for(int i = 0; i < nazioni.length; i++){
+            somma += nazioni[i].getSuperficieKmQ();
+        }
+        if(this.superficieKmQ > somma / nazioni.length){
+            return true;
+        }
+        return false;
+    }
 }
--- Giorno3/src/it/prova/test/TestNazione.java
+++ Giorno3/src/it/prova/test/TestNazione.java
@@ -1,4 +1,46 @@
 package it.prova.test;
 
+import it.prova.model.Nazione;
+
 public class TestNazione {
+    public static void main(String[] args){
+
+        Nazione italia = new Nazione("Italia", 9800.90, 190000);
+        Nazione spagna = new Nazione("Spagna", 8900.14, 210000);
+        Nazione germania = new Nazione("Germania", 11200.75, 830000);
+        Nazione francia = new Nazione("Francia", 10150.30, 670000);
+        Nazione regnoUnito = new Nazione("Regno Unito", 9700.00, 680000);
+        Nazione statiUniti = new Nazione("Stati Uniti", 25000.50, 3310000);
+        Nazione cina = new Nazione("Cina", 17500.40, 14393200);
+        Nazione giappone = new Nazione("Giappone", 10000.20, 1260000);
+        Nazione brasile = new Nazione("Brasile", 8600.60, 2130000);
+        Nazione india = new Nazione("India", 7400.45, 13934000);
+        Nazione canada = new Nazione("Canada", 9300.90, 380000);
+        Nazione australia = new Nazione("Australia", 8700.33, 260000);
+        Nazione[] n1 = {giappone, brasile, australia};
+        Nazione[] n2 = {statiUniti};
+
+
+
+        boolean ePiuEstesa = italia.piuEstesaDi(spagna);
+        System.out.println(ePiuEstesa);
+
+        boolean ePiuPopolosa = cina.piuPopolosaDi(india);
+        System.out.println(ePiuPopolosa);
+
+        boolean almenoUnaPiuEstesa = canada.esisteAlmenoUnaPiuEstesa(n1);
+        System.out.println(almenoUnaPiuEstesa);
+
+        int dimmiQuante = francia.dimmiQuanteSonoPiuPopolose(n2);
+        System.out.println(dimmiQuante);
+
+        boolean haPiuAbitanti = francia.haPiuAbitantiDiTutteLeAltre(n1);
+        System.out.println(haPiuAbitanti);
+
+        boolean stessaIniziale = cina.hannoTutteLaMiaStessaIniziale(n1);
+        System.out.println(stessaIniziale);
+
+        boolean eSopra = cina.eAlDiSopraDellaMediaComeSuperficie(n2);
+        System.out.println(eSopra);
+    }
 }
