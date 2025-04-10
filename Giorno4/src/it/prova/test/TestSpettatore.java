826d0fc7edaa3511395bd1f2f3d558d4dad069d1
unknown <tirotta39@gmail.com>
2025-04-10 13:00:08 +0200

    Implementazione del metodo numeroSpettatoriDelMioStessoSpettacolo con suo relativo test

--- Giorno4/src/it/prova/test/TestSpettatore.java
+++ Giorno4/src/it/prova/test/TestSpettatore.java
@@ -15,5 +15,16 @@ public class TestSpettatore {
         System.out.println("Inizio test incassoDeiPagantiNellaMiaFila...");
         System.out.println(s.incassoDeiPagantiNellaMiaFila(elenco));
         System.out.println("Fine test incassoDeiPagantiNellaMiaFila....");
+
+        Spettatore s1 = new Spettatore("Maria", "Rossi", "4444",
+                new Biglietto("1", 'A', "1", 15));
+
+        Spettatore[] elenco1 = {new Spettatore("Mario", "Rossi", "4444", new Biglietto("1", 'A', "1", 15)),
+                new Spettatore("Paolo", "Rossi", "4444", new Biglietto("1", 'A', "1", 15)),
+                new Spettatore("Luigi", "Rossi", "4444", new Biglietto("1", 'A', "1", 15))};
+
+        System.out.println("Inizio test numeroSpettatoriDelMioStessoSpettacolo...");
+        System.out.println(s1.numeroSpettatoriDelMioStessoSpettacolo(elenco1));
+        System.out.println("Fine test numeroSpettatoriDelMioStessoSpettacolo....");
     }
 }
