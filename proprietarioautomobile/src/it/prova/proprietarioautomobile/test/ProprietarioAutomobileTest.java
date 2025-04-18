4fc8d214c864b4f97466c6c5526f68bae507f4d3
unknown <tirotta39@gmail.com>
2025-04-18 12:02:12 +0200

    Aggiunta test list per proprietario e automobile

--- proprietarioautomobile/src/it/prova/proprietarioautomobile/test/ProprietarioAutomobileTest.java
+++ proprietarioautomobile/src/it/prova/proprietarioautomobile/test/ProprietarioAutomobileTest.java
@@ -8,6 +8,7 @@ import it.prova.proprietarioautomobile.service.automobile.AutomobileService;
 import it.prova.proprietarioautomobile.service.proprietario.ProprietarioService;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class ProprietarioAutomobileTest {
     public static void main(String[] args) {
@@ -24,31 +25,56 @@ public class ProprietarioAutomobileTest {
 //            Proprietario proprietario = new Proprietario("Lorenzo", "Rossi", "1234567890", LocalDate.of(1990, 1, 1));
 //            proprietarioService.inserisciNuovo(proprietario);
 
-//            /// TEST GET AUTOMOBILE
-              Automobile a = automobileService.caricaSingoloAutomobile(3L);
-              System.out.println(a.getMarca());
-              System.out.println(a.getModello());
+////            /// TEST GET AUTOMOBILE
+//              Automobile a = automobileService.caricaSingoloAutomobile(3L);
+//              System.out.println(a.getMarca());
+//              System.out.println(a.getModello());
+//
+////            /// TEST GET PROPRIETARIO
+//              Proprietario p = proprietarioService.caricaSingoloProprietario(1L);
+//              System.out.println(p.getNome());
+//              System.out.println(p.getCognome());
+//
+////            /// TEST AGGIORNAMENTO AUTOMOBILE
+//              a.setMarca("Ford");
+//              a.setModello("Focus");
+//              automobileService.aggiorna(a);
+//
+////            /// TEST AGGIORNAMENTO PROPRIETARIO
+//              p.setNome("Mario");
+//              p.setCognome("Bianchi");
+//              proprietarioService.aggiorna(p);
+//
+////            /// TEST RIMOZIONE AUTOMOBILE
+//              automobileService.rimuovi(3L);
+//
+////            /// TEST RIMOZIONE PROPRIETARIO
+//              proprietarioService.rimuovi(1L);
 
-//            /// TEST GET PROPRIETARIO
-              Proprietario p = proprietarioService.caricaSingoloProprietario(1L);
-              System.out.println(p.getNome());
-              System.out.println(p.getCognome());
+            /// TEST INSERIMENTO AUTOMOBILE
+            Automobile automobile = new Automobile("Fiat", "Panda", "AB123CD", LocalDate.of(2020, 1, 1));
+            automobileService.inserisciNuovo(automobile);
 
-//            /// TEST AGGIORNAMENTO AUTOMOBILE
-              a.setMarca("Ford");
-              a.setModello("Focus");
-              automobileService.aggiorna(a);
+            /// TEST INSERIMENTO PROPRIETARIO
+            Proprietario proprietario = new Proprietario("Lorenzo", "Rossi", "1234567890", LocalDate.of(1990, 1, 1));
+            proprietarioService.inserisciNuovo(proprietario);
 
-//            /// TEST AGGIORNAMENTO PROPRIETARIO
-              p.setNome("Mario");
-              p.setCognome("Bianchi");
-              proprietarioService.aggiorna(p);
+            List<Automobile> results = automobileService.listAllAutomobili();
+            for (Automobile a : results) {
+                System.out.println(a.getMarca());
+                System.out.println(a.getModello());
+                System.out.println(a.getTarga());
+                System.out.println(a.getAnnoImmatricolazione());
+            }
+            List<Proprietario> risultati = proprietarioService.listAllProprietari();
+            for (Proprietario p : risultati) {
+                System.out.println(p.getNome());
+                System.out.println(p.getCognome());
+                System.out.println(p.getCf());
+                System.out.println(p.getDataDiNascita());
+            }
 
-//            /// TEST RIMOZIONE AUTOMOBILE
-              automobileService.rimuovi(3L);
 
-//            /// TEST RIMOZIONE PROPRIETARIO
-              proprietarioService.rimuovi(1L);
 
 
 
